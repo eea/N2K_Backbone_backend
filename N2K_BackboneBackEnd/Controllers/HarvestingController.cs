23b6e93f892a4117c967f5af057b49e3a90c1ee1
bilbomaticaeugis <oesparza@bilbomatica.es>
2022-03-28 15:33:28 +0200

    Agregar archivos de proyecto.

---
+++ N2K_BackboneBackEnd/Controllers/HarvestingController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace N2K_BackboneBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HarvestingController : ControllerBase
+    {
+        // GET: api/<HarvestingController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        
+        [HttpGet]
+        [Route("Harvested")]
+        public IEnumerable<string> Harvested()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        
+        
+        [HttpGet("Harvested/{fromDate}")]
+        public IEnumerable<string> Harvested(DateTime? fromDate)
+        {
+            if (!fromDate.HasValue) return Harvested();
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("Harvested/{fromDate}/{toDate}")]
+        public IEnumerable<string> Harvested(DateTime fromDate, DateTime? toDate)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        [HttpGet]
+        [Route("Pending")]
+        public IEnumerable<string> Pending()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        [HttpGet]
+        [Route("PendingByCountry/{CountryCode}")]
+        public IEnumerable<string> PendingByCountry(string CountryCode)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // GET api/<HarvestingController>/5
+        //Id=>envelopeID
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HarvestingController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HarvestingController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HarvestingController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ N2K_BackboneBackEnd/Models/Harvesting.cs
@@ -0,0 +1,11 @@
+﻿namespace N2K_BackboneBackEnd.Models
+{
+    public class Harvesting
+    {
+
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+    }
+}
