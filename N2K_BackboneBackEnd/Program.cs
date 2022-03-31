86002ac753a68538c394e6336e0c61cb3960269f
bilbomaticaeugis <oesparza@bilbomatica.es>
2022-03-31 08:55:00 +0200

    Return JSON with the same format as the class property name

--- N2K_BackboneBackEnd/Program.cs
+++ N2K_BackboneBackEnd/Program.cs
@@ -6,6 +6,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
 
 var app = builder.Build();
 
