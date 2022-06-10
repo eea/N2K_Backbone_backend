9d91dc8fcc80db2b20a07d18d208b61ece13e537
bilbomaticaeugis <oesparza@bilbomatica.es>
2022-06-10 17:11:36 +0200

    Services to upload and delete files

---
+++ N2K_BackboneBackEnd/Models/AttachedFile.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace N2K_BackboneBackEnd.Models
+{
+    public class AttachedFile : IEntityModel
+    {
+        public string SiteCode { get; set; } = string.Empty;
+        public int Version { get; set; }
+
+        [NotMapped]
+        public IFormFile? File { get; set; }
+
+        public static void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AttachedFile>()
+                .HasNoKey();
+        }
+    }
+}
