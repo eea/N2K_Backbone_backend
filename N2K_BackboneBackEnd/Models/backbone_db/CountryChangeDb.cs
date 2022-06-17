336d7aa6e6b0c2721714acc9403e8bedf8584099
Joana R <joana.rente6@tuta.io>
2022-06-17 14:29:42 +0200

    entity model for the db

---
+++ N2K_BackboneBackEnd/Models/backbone_db/CountryChangeDb.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using N2K_BackboneBackEnd.Enumerations;
+using N2K_BackboneBackEnd.Models.ViewModel;
+
+namespace N2K_BackboneBackEnd.Models.backbone_db
+{
+
+    public class CountryChangeDb : IEntityModel, IEntityModelBackboneDB
+    {
+
+        // TODO
+        public string Country { get; set; }
+
+        public string Code { get; set; }
+
+        public static void OnModelCreating(ModelBuilder builder)
+        {
+            // TODO
+            builder.Entity<CountryChangeDb>()
+                .ToTable("Changes");
+        }
+    }
+}
