a0a92e495b77832dc56e8fd8c67dc44d612492eb
Joana R <joana.rente6@tuta.io>
2022-06-17 14:42:01 +0200

    change name of the interface to match the rest + error correction

---
+++ N2K_BackboneBackEnd/Services/IControllerSiteService.cs
@@ -0,0 +1,11 @@
+﻿using N2K_BackboneBackEnd.Models.backbone_db;
+using Microsoft.AspNetCore.Mvc;
+using N2K_BackboneBackEnd.ServiceResponse;
+
+namespace N2K_BackboneBackEnd.Services
+{
+    public interface IControllerSiteService
+    {
+        Task<ActionResult<ServiceResponse<List<CountryNameCode>>>> GetCountriesWithDataAsync();
+    }
+}
