b101cc5507c661ca72e3a5943493b791bf66fcc6
Joana R <joana.rente6@tuta.io>
2022-06-17 14:28:21 +0200

    add entity model for countries and their codes

---
+++ N2K_BackboneBackEnd/Models/backbone_db/CountryNameCode.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using N2K_BackboneBackEnd.Enumerations;
+using N2K_BackboneBackEnd.Models.versioning_db;
+
+namespace N2K_BackboneBackEnd.Models.backbone_db
+{
+    public class CountryNameCode : IEntityModel
+    {
+        public string? Country { get; set; }
+        public string? Code { get; set; }
+
+        public static void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CountryNameCode>()
+                .ToTable("COUNTRYNAMECODE")
+                .HasKey(c => new {c.Code});
+        }
+    }
+}
