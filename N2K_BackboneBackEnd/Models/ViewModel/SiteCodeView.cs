31e3456f91727113e831c576ef5befc43ab1b1a6
bilbomaticaeugis <oesparza@bilbomatica.es>
2022-08-04 09:30:19 +0200

    Return site name in GetSiteCodesByStatusAndLevelAndCountry

--- N2K_BackboneBackEnd/Models/ViewModel/SiteCodeView.cs
+++ N2K_BackboneBackEnd/Models/ViewModel/SiteCodeView.cs
@@ -9,6 +9,8 @@ namespace N2K_BackboneBackEnd.Models.ViewModel
         public string SiteCode { get; set; } = "";
         public int Version { get; set; }
 
+        public string Name { get; set; } = "";
+
         [NotMapped]
         public string CountryCode { get; set; } = "";
         //public Level Level { get; set; }
