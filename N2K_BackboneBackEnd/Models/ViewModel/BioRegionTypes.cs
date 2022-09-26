3fb8d56228b7ab084390a6b7ba0230ba18d61a76
ALasherasBM <101104809+ALasherasBM@users.noreply.github.com>
2022-09-26 11:43:08 +0200

    Delete unnecesary sp use

--- N2K_BackboneBackEnd/Models/ViewModel/BioRegionTypes.cs
+++ N2K_BackboneBackEnd/Models/ViewModel/BioRegionTypes.cs
@@ -7,6 +7,7 @@ namespace N2K_BackboneBackEnd.Models.ViewModel
         public int Code { get; set; }
         public string? RefBioGeoName { get; set; }
         public string? RefBioRegionCode { get; set; }
+        public string? BioRegionShortCode { get; set; }
         public static void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<BioRegionTypes>()
