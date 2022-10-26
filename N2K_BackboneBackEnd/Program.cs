90951712720398aff6798b25278c8c2e547a2b54
bilbomaticaeugis <oesparza@bilbomatica.es>
2022-10-26 09:28:59 +0200

    Enable token authorisation

--- N2K_BackboneBackEnd/Program.cs
+++ N2K_BackboneBackEnd/Program.cs
@@ -77,8 +77,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 
 builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "N2KBacboneAPI", Version = "v1" });
-    /*
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "N2KBacboneAPI", Version = "v1" });    
     c.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
     {
         Name = "Authorization",
@@ -102,8 +101,7 @@ builder.Services.AddSwaggerGen(c => {
                 },                          
                 new List<string>()
             }
-        });
-    */ 
+    });
 });
 
 builder.Services.AddAuthentication(options =>
