cbe0a5c92c5f36cc058390ad55918992117977db
bilbomaticaeugis <oesparza@bilbomatica.es>
2022-12-01 16:56:39 +0100

    MOdify any comment and file in temporal mode

---
+++ N2K_BackboneBackEnd/Models/backbone_db/DocumentationChanges.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace N2K_BackboneBackEnd.Models.backbone_db
+{
+    public class DocumentationChanges : IEntityModel, IEntityModelBackboneDB
+    {
+        [Key]
+        public long Id { get; set; }
+        public string SiteCode { get; set; } = string.Empty;
+        public int Version { get; set; }
+        public string? Tags { get; set; }
+        [NotMapped]
+        public bool Temporal { get; set; } = false;
+
+        public static void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<StatusChanges>();
+        }
+    }
+
+
+}
--- N2K_BackboneBackEnd/Models/backbone_db/JustificationFiles.cs
+++ N2K_BackboneBackEnd/Models/backbone_db/JustificationFiles.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace N2K_BackboneBackEnd.Models.backbone_db
 {
-    public class JustificationFiles : IEntityModel, IEntityModelBackboneDB
+    public class JustificationFiles :  DocumentationChanges, IEntityModel, IEntityModelBackboneDB
     {
-        [Key]
-        public long Id { get; set; }
+        //[Key]
+        //public long Id { get; set; }
 
-        public string SiteCode { get; set; } = string.Empty;
-        public int Version { get; set; }
+        //public string SiteCode { get; set; } = string.Empty;
+        //public int Version { get; set; }
 
         public String? Path { get; set; }
 
@@ -19,8 +19,12 @@ namespace N2K_BackboneBackEnd.Models.backbone_db
         public string? Username { get; set; }
 
         [NotMapped]
-        public bool Temporal { get; set; } = false;
-        public static void OnModelCreating(ModelBuilder builder)
+        public new string Tags { get; set; } = "";
+
+
+        //[NotMapped]
+        //public bool Temporal { get; set; } = false;
+        public static new void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<JustificationFiles>()
                 .ToTable("JustificationFiles");
