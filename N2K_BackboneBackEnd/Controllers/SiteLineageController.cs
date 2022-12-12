373d33635441c7192a6d7481f4c5a985917726b8
ALasherasBM <101104809+ALasherasBM@users.noreply.github.com>
2022-12-12 13:21:27 +0100

    Create SiteLineage Controller

---
+++ N2K_BackboneBackEnd/Controllers/SiteLineageController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using N2K_BackboneBackEnd.Models;
+using N2K_BackboneBackEnd.ServiceResponse;
+using AutoMapper;
+using N2K_BackboneBackEnd.Services;
+using N2K_BackboneBackEnd.Models.ViewModel;
+using N2K_BackboneBackEnd.Enumerations;
+using N2K_BackboneBackEnd.Models.backbone_db;
+using Microsoft.AspNetCore.Authorization;
+
+namespace N2K_BackboneBackEnd.Controllers
+{
+    [Authorize(AuthenticationSchemes = "EULoginSchema")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SiteLineageController : ControllerBase
+    {
+        private readonly ISiteLineageService _siteLineageService;
+        private readonly IMapper _mapper;
+        private IMemoryCache _cache;
+
+
+        public SiteLineageController(ISiteLineageService siteLineageService, IMapper mapper,IMemoryCache cache)
+        {
+            _siteLineageService = siteLineageService;
+            _mapper = mapper;
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+
+        [HttpGet("GetSiteLineage")]
+        public async Task<ActionResult<ServiceResponse<List<SiteLineage>>>> Get(string siteCode)
+        {
+            var response = new ServiceResponse<List<SiteLineage>>();
+            try
+            {
+                var siteLineage = await _siteLineageService.GetSiteLineageAsync(siteCode);
+                response.Success = true;
+                response.Message = "";
+                response.Data = siteLineage;
+                response.Count = (siteLineage == null) ? 0 : siteLineage.Count;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+                response.Count = 0;
+                response.Data = new List<SiteLineage>();
+                return Ok(response);
+            }
+        }
+    }
+}
---
+++ N2K_BackboneBackEnd/Services/SiteLineageService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using N2K_BackboneBackEnd.Data;
+using N2K_BackboneBackEnd.Models;
+using N2K_BackboneBackEnd.Models.ViewModel;
+using N2K_BackboneBackEnd.Models.backbone_db;
+using N2K_BackboneBackEnd.Enumerations;
+using N2K_BackboneBackEnd.Models.versioning_db;
+using System.Data;
+using NuGet.Protocol;
+using N2K_BackboneBackEnd.Helpers;
+using System.Security.Policy;
+using System.Collections.Generic;
+
+namespace N2K_BackboneBackEnd.Services
+{
+
+
+    public class SiteLineageService : ISiteLineageService
+    {
+        private readonly N2KBackboneContext _dataContext;
+
+
+        public SiteLineageService(N2KBackboneContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+
+        public async Task<List<SiteLineage>> GetSiteLineageAsync(string siteCode)
+        {
+            List<SiteLineage> result;
+            return result;
+        }
+
+        
+
+    }
+}
