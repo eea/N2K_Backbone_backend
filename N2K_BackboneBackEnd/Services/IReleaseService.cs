a4bcc0ae350b524e0a706a075b1133c772355e10
ALasherasBM <101104809+ALasherasBM@users.noreply.github.com>
2022-12-20 11:53:04 +0100

    Fix output model inconsistencies in release services

--- N2K_BackboneBackEnd/Services/IReleaseService.cs
+++ N2K_BackboneBackEnd/Services/IReleaseService.cs
@@ -7,13 +7,13 @@ namespace N2K_BackboneBackEnd.Services
     public interface IReleaseService
     {
         Task<List<BioRegionTypes>> GetUnionBioRegionTypes();
-        Task<List<UnionListHeader>> GetReleaseHeadersByBioRegion(string? bioRegionShortCode);
+        Task<List<Releases>> GetReleaseHeadersByBioRegion(string? bioRegionShortCode);
         Task<List<ReleaseDetail>> GetCurrentSitesReleaseDetailByBioRegion(string? bioRegionShortCode);
         Task<List<Releases>> GetReleaseHeadersById(long? id);
         Task<UnionListComparerSummaryViewModel> GetCompareSummary(long? idSource, long? idTarget, string? bioRegions, IMemoryCache cache);
         Task<List<UnionListComparerDetailedViewModel>> CompareReleases(long? idSource, long? idTarget, string? bioRegions, string? country, IMemoryCache cache, int page = 1, int pageLimit = 0);
-        Task<List<UnionListHeader>> CreateRelease(string title, Boolean? isOfficial, string? character, string? comments);
-        Task<List<UnionListHeader>> UpdateRelease(long id, string name, Boolean final);
+        Task<List<Releases>> CreateRelease(string title, Boolean? isOfficial, string? character, string? comments);
+        Task<List<Releases>> UpdateRelease(long id, string name, Boolean final);
         Task<int> DeleteRelease(long id);
     }
 }
