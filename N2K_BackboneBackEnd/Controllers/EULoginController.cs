2e8bf1952a1901b92d154b7d01a4979ada547ad9
bilbomaticaeugis <oesparza@bilbomatica.es>
2023-02-20 16:27:26 +0100

    GetToken method with POST method and relevant classes

--- N2K_BackboneBackEnd/Controllers/EULoginController.cs
+++ N2K_BackboneBackEnd/Controllers/EULoginController.cs
@@ -18,22 +18,22 @@ namespace N2K_BackboneBackEnd.Controllers
         private readonly IMapper _mapper;
 
 
-        public EULoginController(IOptions<ConfigSettings> app, IEULoginService euLoginService, IMapper mapper )
+        public EULoginController(IOptions<ConfigSettings> app, IEULoginService euLoginService, IMapper mapper)
         {
             _appSettings = app;
             _euLoginService = euLoginService;
-            _mapper= mapper;
+            _mapper = mapper;
         }
 
         [System.Web.Http.AllowAnonymous]
         [HttpGet("GetLoginUrl/redirectionUrl={redirectionUrl}")]
-        public async Task<ActionResult<ServiceResponse<string>>> GetLoginUrl (string redirectionUrl)
+        public async Task<ActionResult<ServiceResponse<string>>> GetLoginUrl(string redirectionUrl)
         {
 
             var stringUrl = redirectionUrl.Replace("##", "/").Replace("%2F", "/");
             var response = new ServiceResponse<string>();
             try
-            {              
+            {
                 var url = await _euLoginService.GetLoginUrl(stringUrl);
                 response.Success = true;
                 response.Message = "";
@@ -59,7 +59,7 @@ namespace N2K_BackboneBackEnd.Controllers
         /// <param name="code_challenge">A code challenge generated in javascript via base64URL(CryptoJS.SHA256(code_verifier)))</param>
         [System.Web.Http.AllowAnonymous]
         [HttpGet("GetLoginUrlByCodeChallenge/redirectionUrl={redirectionUrl}&code_challenge={code_challenge}")]
-        public async Task<ActionResult<ServiceResponse<string>>> GetLoginUrl(string redirectionUrl,string code_challenge)
+        public async Task<ActionResult<ServiceResponse<string>>> GetLoginUrl(string redirectionUrl, string code_challenge)
         {
             var stringUrl = redirectionUrl.Replace("##", "/").Replace("%2F", "/");
             var response = new ServiceResponse<string>();
@@ -83,6 +83,7 @@ namespace N2K_BackboneBackEnd.Controllers
         }
 
 
+        /*
         [System.Web.Http.AllowAnonymous]
         [HttpGet("GetToken/redirectionUrl={redirectionUrl}&code={code}&code_verifier={code_verifier}")]
         public async Task<ActionResult<ServiceResponse<string>>> GetToken(string redirectionUrl, string code, string code_verifier)
@@ -108,10 +109,39 @@ namespace N2K_BackboneBackEnd.Controllers
             }
 
         }
+        */
+
+
+        [System.Web.Http.AllowAnonymous]
+        [Route("GetToken/")]
+        [HttpPost]
+        public async Task<ActionResult<List<ModifiedSiteCode>>> GetToken([FromBody] GetTokenParams tokenParams)
+        {
+            var response = new ServiceResponse<string>();
+            var stringUrl = tokenParams.RedirectionUrl.Replace("##", "/").Replace("%2F", "/");
+            try
+            {
+                var url = await _euLoginService.GetToken(stringUrl, tokenParams.Code, tokenParams.Code_Verifier);
+                response.Success = true;
+                response.Message = "";
+                response.Data = url;
+                response.Count = (url == null) ? 0 : 1;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+                response.Count = 0;
+                response.Data = "";
+                return Ok(response);
+            }
+
+        }
 
         [System.Web.Http.AllowAnonymous]
         [HttpGet("GetUsername/token={token}")]
-        public async Task<ActionResult<ServiceResponse<string>>> GetUsername(string token)
+        public async Task<ActionResult<ServiceResponse<string>>> UsernameGet(string token)
         {
             var response = new ServiceResponse<string>();
             try
@@ -133,6 +163,32 @@ namespace N2K_BackboneBackEnd.Controllers
             }
 
         }
+
+        [System.Web.Http.AllowAnonymous]
+        [HttpPost("GetUsername")]
+        public async Task<ActionResult<ServiceResponse<string>>> UsernamePost([FromBody] GetUsernameParams tokenParams)
+        {
+            var response = new ServiceResponse<string>();
+            try
+            {
+                var url = await _euLoginService.GetUsername(tokenParams.Token);
+                response.Success = true;
+                response.Message = "";
+                response.Data = url;
+                response.Count = (url == null) ? 0 : 1;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+                response.Count = 0;
+                response.Data = "";
+                return Ok(response);
+            }
+
+        }
+
 
 
     }
