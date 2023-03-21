09c63a4806621db05cb2efddac0b104034f3c73f
ALasherasBM <101104809+ALasherasBM@users.noreply.github.com>
2023-03-21 13:17:49 +0100

    Prevent double validation

--- N2K_BackboneBackEnd/Services/HarvestedService.cs
+++ N2K_BackboneBackEnd/Services/HarvestedService.cs
@@ -902,10 +902,6 @@ namespace N2K_BackboneBackEnd.Services
                             //When there is no previous envelopes to resolve for this country
                             if (envelopes.Count == 0)
                             {
-                                Task tabValidationTask = Validate(_tempEnvelope);
-                                Task spatialValidationTask = ValidateSpatialData(_tempEnvelope);
-                                //make sure they are all finished
-                                await Task.WhenAll(tabValidationTask, spatialValidationTask);
                                 //change the status of the whole process to PreHarvested
                                 await ChangeStatus(envelope.CountryCode, envelope.VersionId, HarvestingStatus.PreHarvested, cache);
                                 bbEnvelope[0].Status = SiteChangeStatus.PreHarvested;
@@ -998,11 +994,18 @@ namespace N2K_BackboneBackEnd.Services
                     {
                         if (envelope.Status == HarvestingStatus.DataLoaded)
                         {
-                            await Validate(new EnvelopesToProcess[] { new EnvelopesToProcess
+                            Task tabValidationTask = Validate(new EnvelopesToProcess[] { new EnvelopesToProcess
                             {
                                 CountryCode = country,
                                 VersionId = version
                             } });
+                            Task spatialValidationTask = ValidateSpatialData(new EnvelopesToProcess[] { new EnvelopesToProcess
+                            {
+                                CountryCode = country,
+                                VersionId = version
+                            } });
+                            //make sure they are all finished
+                            await Task.WhenAll(tabValidationTask, spatialValidationTask);
                         }
 
                         SqlParameter param1 = new SqlParameter("@country", country);
@@ -1041,10 +1044,13 @@ namespace N2K_BackboneBackEnd.Services
                                     SubmissionDate = DateTime.Now
                                 };
                                 EnvelopesToProcess[] _tempEnvelope = new EnvelopesToProcess[] { nextEnvelopeToValidate };
-                                Task tabValidationTask = Validate(_tempEnvelope);
-                                Task spatialValidationTask = ValidateSpatialData(_tempEnvelope);
-                                //make sure they are all finished
-                                await Task.WhenAll(tabValidationTask, spatialValidationTask);
+                                if (nextEnvelope.Status != HarvestingStatus.DataLoaded)
+                                {
+                                    Task tabValidationTask = Validate(_tempEnvelope);
+                                    Task spatialValidationTask = ValidateSpatialData(_tempEnvelope);
+                                    //make sure they are all finished
+                                    await Task.WhenAll(tabValidationTask, spatialValidationTask);
+                                }
                                 //change the status of the whole process to PreHarvested
                                 await ChangeStatus(nextEnvelope.Country, nextEnvelope.Version, HarvestingStatus.PreHarvested, cache);
                             }
