9f3e2279a461893d0911fb960e1ecf1f36ef3423
ALasherasBM <101104809+ALasherasBM@users.noreply.github.com>
2023-04-25 13:46:38 +0200

    Change Lineage Type and Status to enums

---
+++ N2K_BackboneBackEnd/Enumerations/LineageStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace N2K_BackboneBackEnd.Enumerations
+{
+    [DataContract]
+    public enum LineageStatus
+    {
+        [DataMember]
+        Proposed = 0,
+        [DataMember]
+        Consolidated = 1
+
+    }
+}
