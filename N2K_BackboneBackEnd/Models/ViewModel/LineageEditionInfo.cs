53630402977ce8d1aa246f03141d90eb823fe4f9
ALasherasBM <101104809+ALasherasBM@users.noreply.github.com>
2023-04-27 13:05:41 +0200

    Create LineageEditionInfo.cs

---
+++ N2K_BackboneBackEnd/Models/ViewModel/LineageEditionInfo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using N2K_BackboneBackEnd.Enumerations;
+using N2K_BackboneBackEnd.Helpers;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace N2K_BackboneBackEnd.Models.backbone_db
+{
+    [Keyless]
+    public class LineageEditionInfo : IEntityModel
+    {
+        public string SiteCode { get; set; } = string.Empty;
+        public string SiteName { get; set; } = string.Empty;
+        public string SiteType { get; set; } = string.Empty;
+        public string? BioRegion { get; set; }
+        public double? AreaSDF { get; set; }
+        public double? AreaGEO { get; set; }
+        public double? Length { get; set; }
+    }
+}
