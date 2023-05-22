9ece9b0180ae518e7f51dc9aaade6803cb2b284d
bilbomaticaeugis <oesparza@bilbomatica.es>
2023-05-22 09:20:09 +0200

    Recover concurrent task for harvesting

---
+++ N2K_BackboneBackEnd/Services/IBackgroundSpatialHarvestJobs.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Options;
+using N2K_BackboneBackEnd.Data;
+using N2K_BackboneBackEnd.Models;
+
+namespace N2K_BackboneBackEnd.Services
+{
+    public interface IBackgroundSpatialHarvestJobs
+    {
+        event EventHandler<FMEJobEventArgs> FMEJobCompleted;
+        void CheckFMEJobsStatus(IOptions<ConfigSettings> appSettings);
+        Task LaunchFMESpatialHarvestBackground(EnvelopesToProcess envelope);
+
+        N2KBackboneContext GetDataContext();
+    }
+}
