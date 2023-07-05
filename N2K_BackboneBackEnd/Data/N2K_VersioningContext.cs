4cdf23de87b269ab2d5ea8b7b4511dd08f8f3acb
bilbomaticaeugis <oesparza@bilbomatica.es>
2023-07-05 11:31:24 +0200

    Add timeout in context creation clases

--- N2K_BackboneBackEnd/Data/N2K_VersioningContext.cs
+++ N2K_BackboneBackEnd/Data/N2K_VersioningContext.cs
@@ -8,7 +8,7 @@ namespace N2K_BackboneBackEnd.Data
         //public DbSet<OwnerType> OwnerType { get; set; }
 
         public N2K_VersioningContext(DbContextOptions<N2K_VersioningContext> options) : base(options) {
-          
+            this.Database.SetCommandTimeout(600);
         }
 
     }
