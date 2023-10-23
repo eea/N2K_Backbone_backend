dc2a81469ec210ce4420623b2b6325bb95589a9c
ALasherasBM <101104809+ALasherasBM@users.noreply.github.com>
2023-10-23 09:24:26 +0200

    Add geometry new/missing change

---
+++ N2K_BackboneBackEnd/Models/ViewModel/SiteSpatialBasic.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using N2K_BackboneBackEnd.Enumerations;
+using N2K_BackboneBackEnd.Helpers;
+using N2K_BackboneBackEnd.Models.backbone_db;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace N2K_BackboneBackEnd.Models
+{
+    [Keyless]
+    public class SiteSpatialBasic : IEntityModel
+    {
+        public string SiteCode { get; set; } = string.Empty;
+        public int Version { get; set; }
+        public Boolean? data { get; set; }
+        public decimal? area { get; set; }
+        public static void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<SiteSpatialBasic>();
+        }
+    }
+}
