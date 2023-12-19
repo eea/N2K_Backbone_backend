ec332da49ba8b4cb26d83eb08bcde54f9d6a73bc
bilbomaticaeugis <oesparza@bilbomatica.es>
2023-12-19 08:00:32 +0100

    Check if storedSite is not null

--- N2K_BackboneBackEnd/Services/HarvestedService.cs
+++ N2K_BackboneBackEnd/Services/HarvestedService.cs
@@ -928,7 +928,8 @@ namespace N2K_BackboneBackEnd.Services
                     if (siteRelation != null)
                     {
                         storedSite = referencedSites.Where(s => s.SiteCode == siteRelation.PreviousSiteCode && s.VersionId == siteRelation.PreviousVersion).FirstOrDefault();
-                        ld = detectedLineageChanges.FirstOrDefault(e => e.new_sitecode == storedSite.SiteCode && e.new_version == storedSite.VersionId);
+                        if (storedSite != null) 
+                            ld = detectedLineageChanges.FirstOrDefault(e => e.new_sitecode == storedSite.SiteCode && e.new_version == storedSite.VersionId);
                     }
                     if (siteRelation != null && storedSite != null)
                     {
