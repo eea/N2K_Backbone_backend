89bdf1b59e40c898432b6a258c00bf84031b691c
ALasherasBM <101104809+ALasherasBM@users.noreply.github.com>
2024-01-17 12:39:52 +0100

    Misc models code optimized

--- N2K_BackboneBackEnd/Models/GetTokenParams.cs
+++ N2K_BackboneBackEnd/Models/GetTokenParams.cs
@@ -2,14 +2,13 @@
 {
     public class GetTokenParams
     {
-        public string RedirectionUrl { get; set; } = "";
-        public string Code { get; set; } = "";
-        public string Code_Verifier { get; set; } = "";
+        public string RedirectionUrl { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string Code_Verifier { get; set; } = string.Empty;
     }
 
     public class GetUsernameParams
     {
-        public string Token { get; set; } = "";
-
+        public string Token { get; set; } = string.Empty;
     }
 }
