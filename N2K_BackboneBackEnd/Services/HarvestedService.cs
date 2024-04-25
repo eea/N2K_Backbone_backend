36c30cd669e073577315762aa509c7c463cc49d8
bilbomaticaeugis <oesparza@bilbomatica.es>
2024-04-25 15:12:52 +0200

    Add semaphore in _fmeHarvestJobs.FMEJobCompleted event handler

--- N2K_BackboneBackEnd/Services/HarvestedService.cs
+++ N2K_BackboneBackEnd/Services/HarvestedService.cs
@@ -34,9 +34,10 @@ namespace N2K_BackboneBackEnd.Services
         private IList<Models.backbone_db.SpecieBase> _countrySpecies = new List<Models.backbone_db.SpecieBase>();
 
         private readonly IHubContext<ChatHub> _hubContext;
+        private static SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
 
         //private static readonly SemaphoreSlim _semaphore = new SemaphoreSlim(initialCount:1);
-        //private static readonly SemaphoreSlim _semaphoreFME = new SemaphoreSlim(initialCount: 1);
+
         private IDictionary<Type, object> _siteItems = new Dictionary<Type, object>(); private struct SiteVersion
         {
             public string SiteCode;
@@ -1689,6 +1690,9 @@ namespace N2K_BackboneBackEnd.Services
 
                     await SystemLog.WriteAsync(SystemLog.errorLevel.Info, string.Format("Enter Event handler with fme job {0}-{1}", env.Envelope.CountryCode, env.Envelope.VersionId), "EventHandler", "", _connectionString);
 
+
+                    //make sure the execution completes until it starts a new one
+                    await _semaphoreSlim.WaitAsync();
                     try
                     {
                         //avoid handling the same event more than once by the means of memory cache
@@ -1701,6 +1705,7 @@ namespace N2K_BackboneBackEnd.Services
                         //if the file exists means that the event was handled and we ignore it
                         if (!File.Exists(fileName))
                         {
+
                             await SystemLog.WriteAsync(SystemLog.errorLevel.Info, string.Format("Event handler file {0}", fileName), "EventHandler", "", _connectionString);
                             //if it doesn´t exist create a file
                             //await _semaphoreFME.WaitAsync();
@@ -1718,6 +1723,8 @@ namespace N2K_BackboneBackEnd.Services
                     {
                         await SystemLog.WriteAsync(SystemLog.errorLevel.Error, string.Format("Error Event handler {0}", ex.Message), "EventHandler", "", _connectionString);
                     }
+                    _semaphoreSlim.Release();
+
                 };
             }
             catch (Exception ex)
@@ -1754,6 +1761,7 @@ namespace N2K_BackboneBackEnd.Services
                             return;
 
                         Console.WriteLine(String.Format("Harvest spatial {0}-{1} completed", env.Envelope.CountryCode, env.Envelope.VersionId));
+                        await SystemLog.WriteAsync(SystemLog.errorLevel.Info, string.Format("Harvest spatial {0}-{1} completed", env.Envelope.CountryCode, env.Envelope.VersionId), "FMEJobCompleted", "", _connectionString);
 
                         if (_procEnv.Status == HarvestingStatus.TabularDataLoaded)
                             _procEnv.Status = HarvestingStatus.DataLoaded;
