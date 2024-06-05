3bcac7833973ca9419af88fcf6232dde4e7e79f2
Joana R <joana.rente6@tuta.io>
2024-06-05 12:06:04 +0200

    add new public service and exclude sensitive species from it

--- N2K_BackboneBackEnd/Controllers/SDFController.cs
+++ N2K_BackboneBackEnd/Controllers/SDFController.cs
@@ -93,4 +93,45 @@ namespace N2K_BackboneBackEnd.Controllers
             }
         }
     }
+    
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicSDFController : ControllerBase
+    {
+        private readonly ISDFService _SDFService;
+        private readonly IMapper _mapper;
+
+        public PublicSDFController(ISDFService SDFService, IMapper mapper)
+        {
+            _SDFService = SDFService;
+            _mapper = mapper;
+        }
+        
+        [AllowAnonymous]
+        [Route("GetPublicReleaseData")]
+        [HttpGet]
+        public async Task<ActionResult<ReleaseSDF>> GetReleaseData(string SiteCode, int ReleaseId = -1)
+        {
+            ServiceResponse<ReleaseSDF> response = new();
+            try
+            {
+                ReleaseSDF result = await _SDFService.GetReleaseData(SiteCode, ReleaseId, false);
+                response.Success = true;
+                response.Message = "";
+                response.Data = result;
+                response.Count = 1;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+                response.Count = 0;
+                response.Data = new ReleaseSDF();
+                return Ok(response);
+            }
+        }
+        
+    }
 }
