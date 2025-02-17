0abfd4f0ecbbb92c567bd071d8d48f528dc39704
Oskar Esparza <oesparza@bilbomatica.es>
2025-02-17 16:07:45 +0100

    Dummy change for deployment  CI/CD test

--- N2K_BackboneBackEnd/Controllers/PublicDataController.cs
+++ N2K_BackboneBackEnd/Controllers/PublicDataController.cs
@@ -24,12 +24,12 @@ namespace N2K_BackboneBackEnd.Controllers
         [AllowAnonymous]
         [Route("GetReleaseData")]
         [HttpGet]
-        public async Task<ActionResult<ReleaseSDF>> GetReleaseData(string SiteCode, int ReleaseId = -1, Boolean initialValidation = false, Boolean internalViewers = false, Boolean internalBarometer = false, Boolean internalPortalSDFSensitive = false, Boolean publicViewers = false, Boolean publicBarometer = false, Boolean sdfPublic = false, Boolean naturaOnlineList = false, Boolean productsCreated = false, Boolean jediDimensionCreated = false)
+        public async Task<ActionResult<ReleaseSDF>> GetReleaseData(string SiteCode, int ReleaseId = -1, Boolean initialValidation = false, Boolean internalViewers = false, Boolean internalBarometer = false, Boolean internalPortalSDFSensitive = false, Boolean publicViewers = false, Boolean publicBarometer = false, Boolean sdfPublic = false, Boolean naturaOnlineList = false, Boolean productsCreated = false, Boolean jediDimensionCreatedA = false)
         {
             ServiceResponse<ReleaseSDF> response = new();
             try
             {
-                ReleaseSDF result = await _SDFService.GetReleaseData(SiteCode, ReleaseId, initialValidation, internalViewers, internalBarometer, internalPortalSDFSensitive, publicViewers, publicBarometer, sdfPublic, naturaOnlineList, productsCreated, jediDimensionCreated);
+                ReleaseSDF result = await _SDFService.GetReleaseData(SiteCode, ReleaseId, initialValidation, internalViewers, internalBarometer, internalPortalSDFSensitive, publicViewers, publicBarometer, sdfPublic, naturaOnlineList, productsCreated, jediDimensionCreatedA);
                 response.Success = true;
                 response.Message = "";
                 response.Data = result;
